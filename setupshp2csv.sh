18ee280194e0d52db25b65b05cf1feae7920d485
hgrandy94 <hegrandy@microsoft.com>
2017-12-20 03:47:10 +0000

    Fixed curl calls and paths. NEED TO CALL source ~/.bashrc after running setup script now.

--- setupshp2csv.sh
+++ setupshp2csv.sh
@@ -7,15 +7,15 @@ sudo apt install unixodbc
 sudo apt install unixodbc-dev
 
 # Install ODBC driver 13 for SQL
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -'
+sudo sh -c 'curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list'
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$PATH:/opt/mssql-tools/bin"
 
 # Download and install mini conda
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O ~/miniconda.sh
@@ -25,6 +25,7 @@ bash ~/miniconda.sh -b -f -p $HOME/miniconda
 # Add miniconda to path
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+export PATH="$HOME/miniconda/bin:$PATH"
 
 
 # Run pip install cmds for ADLS and sql
